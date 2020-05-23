e2484f61d9bdabf85944e0e3a580267ab3b970db
Holumide <ajayimide420@gmail.com>
2020-05-23 21:05:48 +0100

    quick change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ const routes: Routes = [
   {path:'home', component: HomeComponent},
   { path: 'about', component: AboutComponent },
   {path:'shop', component:ShopComponent},
-  {path:'refrence', component:RefrenceComponent}
+  {path:'refrence', component:RefrenceComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
   // { path: 'secondary', component: SecondaryComponent },
 ];
 
